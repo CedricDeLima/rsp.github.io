896ea38a5425f794a680b7d4586a8ab9bf9500a4
Cédric De Lima <157008330+CedricDeLima@users.noreply.github.com>
2026-06-12 15:52:02 +0200

    Update helpers.js

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -1,1 +1,18 @@
+export function toSlug(name) {
+  return name
+    .toLowerCase()
+    .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-|-$/g, "")
+    .slice(0, 40);
+}
+
+export function esc(str) {
+  if (!str) return '';
+  return String(str)
+    .replace(/&/g,'&')
+    .replace(/</g,'<')
+    .replace(/>/g,'>')
+    .replace(/"/g,'"');
+}
 
