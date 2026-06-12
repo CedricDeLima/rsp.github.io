fbef295f36d3aa902787ed021dbac2b26144e7e3
Cédric De Lima <157008330+CedricDeLima@users.noreply.github.com>
2026-06-12 16:08:44 +0200

    Update supabaseClient.js

--- js/services/supabaseClient.js
+++ js/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL = "https://xrtmlddmoepmomvtjgok.supabase.co";
-const SUPABASE_KEY = "TA_PUBLIC_KEY";
+const SUPABASE_KEY = "sb_publishable_xrDJju9cKJFoyMT1nRcDkQ_dy7doWL9";
 
 export const db = createClient(SUPABASE_URL, SUPABASE_KEY);
